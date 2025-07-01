27d66a1249bf06fb306a7dc2102fe6bd7902e951
MixXLs <mixxs12345@gmail.com>
2025-07-01 16:02:01 +0700

    End Lecture-02

---
+++ Lecture-01/testprintnum.py
@@ -0,0 +1,13 @@
+from math import pi
+
+print("100.00")
+print("%d,%100")
+print("%d, %100.58")
+print("%f" %-100)
+print("%.2f" %100.658)
+print("%.4f" %pi)
+
+print("My age is "+ str(25) +" I have " + str(3500.50)+"Bth.")
+
+print("5+4 = " + str(5+4))
+print("5+4 =",5+4)
