4c3cb4b35323f2be5fe6d00a40eb1ae56a0681ff
MixXLs <mixxs12345@gmail.com>
2025-07-08 16:00:02 +0700

    Lc03

---
+++ Lecture-03/logical_operators.py
@@ -0,0 +1,12 @@
+x = 10
+y = 20 
+z = 30
+
+if x < y and y < z:
+    print("x is less than y and y is less tham z.") # True
+
+if x or y > z:
+    print("Either x is less than y or y is greater than z.") #True
+
+if not (x > y):
+    print("x is not greter than y.") #True
